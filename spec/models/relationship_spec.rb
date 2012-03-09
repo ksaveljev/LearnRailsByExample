93400a3cd216f1182351fcdcda0710b812d832ac
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2012-03-09 17:36:42 +0200

    Many to many relationship between following and followed users

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Relationship do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:follower) { FactoryGirl.create(:user) }
+  let(:followed) { FactoryGirl.create(:user) }
+  let(:relationship) do
+    follower.relationships.build(followed_id: followed.id)
+  end
+
+  subject { relationship }
+
+  it { should be_valid }
+
+  describe "follower methods" do
+    before { relationship.save }
+
+    it { should respond_to(:follower) }
+    it { should respond_to(:followed) }
+    its(:follower) { should == follower }
+    its(:followed) { should == followed }
+  end
+
+  describe "when followed id is not present" do
+    before { relationship.followed_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when follower id is not present" do
+    before { relationship.follower_id = nil }
+    it { should_not be_valid }
+  end
 end
